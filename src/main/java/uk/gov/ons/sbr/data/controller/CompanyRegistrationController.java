bd04f85f23a55e0a76a160981885b79308f9d2fc
Adrian Harris <harrih@C02SQ033G8WL.local>
2017-08-08 07:42:30 +0100

    Removed unused imports

--- src/main/java/uk/gov/ons/sbr/data/controller/CompanyRegistrationController.java
+++ src/main/java/uk/gov/ons/sbr/data/controller/CompanyRegistrationController.java
@@ -1,20 +1,12 @@
 package uk.gov.ons.sbr.data.controller;
 
 import uk.gov.ons.sbr.data.dao.CompanyRegistrationDAO;
-import uk.gov.ons.sbr.data.dao.EnterpriseDAO;
-import uk.gov.ons.sbr.data.dao.UnitDAO;
 import uk.gov.ons.sbr.data.domain.CompanyRegistration;
-import uk.gov.ons.sbr.data.domain.Enterprise;
-import uk.gov.ons.sbr.data.domain.UnitLinks;
-import uk.gov.ons.sbr.data.domain.UnitType;
 import uk.gov.ons.sbr.data.hbase.dao.HBaseCompanyRegistrationDAO;
-import uk.gov.ons.sbr.data.hbase.dao.HBaseEnterpriseDAO;
-import uk.gov.ons.sbr.data.hbase.dao.HBaseUnitDAO;
 import uk.gov.ons.sbr.data.hbase.util.ReferencePeriodUtils;
 
 import java.io.IOException;
 import java.time.YearMonth;
-import java.util.Map;
 import java.util.Optional;
 
 /**
