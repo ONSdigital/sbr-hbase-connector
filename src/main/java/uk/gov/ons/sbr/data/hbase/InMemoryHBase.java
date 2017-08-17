9fa31fa657b5dbd001872857f966c9eae072588c
Adrian Harris <harrih@C02SQ033G8WL.local>
2017-08-17 10:02:05 +0100

    Fixed core overlapping dependencies
    - Fixed “commons” overlapping dependencies
    - Prevented HBaseTestUtility from logging by removing log4j
    - Sorted the pom.xml
    - Deleted dependency-reduced-pom.xml as not used

--- src/main/java/uk/gov/ons/sbr/data/hbase/InMemoryHBase.java
+++ src/main/java/uk/gov/ons/sbr/data/hbase/InMemoryHBase.java
@@ -2,6 +2,9 @@ package uk.gov.ons.sbr.data.hbase;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.ons.sbr.data.hbase.dao.HBaseEnterpriseDAO;
 import uk.gov.ons.sbr.data.hbase.table.ColumnFamilies;
 import uk.gov.ons.sbr.data.hbase.table.TableNames;
 
@@ -10,10 +13,12 @@ import uk.gov.ons.sbr.data.hbase.table.TableNames;
  */
 public class InMemoryHBase {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HBaseEnterpriseDAO.class.getName());
     private static HBaseTestingUtility hBaseTestingUtility;
 
     public static Configuration init() throws Exception {
         if (hBaseTestingUtility == null) {
+            LOG.info("Starting in memory Hbase instance...");
             hBaseTestingUtility = new HBaseTestingUtility();
             hBaseTestingUtility.setJobWithoutMRCluster();
             hBaseTestingUtility.startMiniCluster();
@@ -22,17 +27,20 @@ public class InMemoryHBase {
             hBaseTestingUtility.createTable(TableNames.COMPANIES_HOUSE_DATA.getTableName(), ColumnFamilies.COMPANY_DATA.getColumnFamily());
             hBaseTestingUtility.createTable(TableNames.VAT.getTableName(), ColumnFamilies.VAT_DATA.getColumnFamily());
             hBaseTestingUtility.createTable(TableNames.PAYE.getTableName(), ColumnFamilies.PAYE_DATA.getColumnFamily());
+            LOG.info("In memory Hbase instance started");
         }
         return hBaseTestingUtility.getConfiguration();
     }
 
     @Override
     protected void finalize() throws Throwable {
+        LOG.info("Stopping in memory Hbase instance...");
         hBaseTestingUtility.cleanupDataTestDirOnTestFS(TableNames.ENTERPRISE.getTableName().getNameAsString());
         hBaseTestingUtility.cleanupDataTestDirOnTestFS(TableNames.UNIT_LINKS.getTableName().getNameAsString());
         hBaseTestingUtility.cleanupDataTestDirOnTestFS(TableNames.COMPANIES_HOUSE_DATA.getTableName().getNameAsString());
         hBaseTestingUtility.cleanupDataTestDirOnTestFS(TableNames.VAT.getTableName().getNameAsString());
         hBaseTestingUtility.cleanupDataTestDirOnTestFS(TableNames.PAYE.getTableName().getNameAsString());
         hBaseTestingUtility.shutdownMiniCluster();
+        LOG.info("In memory Hbase instance shutdown");
     }
 }
